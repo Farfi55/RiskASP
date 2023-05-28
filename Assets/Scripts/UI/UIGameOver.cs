9023cef43c99459b279082509a037216f26616bb
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-05-29 00:17:15 +0200

    Game Over Message

---
+++ Assets/Scripts/UI/UIGameOver.cs
@@ -0,0 +1,53 @@
+using System;
+using player;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIGameOver : MonoBehaviour
+    {
+        private GameManager _gameManager;
+        
+        [SerializeField] private Image _backgroundTintImage;
+        [SerializeField] private TMP_Text _gameOverInfoText;
+
+
+        private void Awake()
+        {
+            
+            _gameManager = GameManager.Instance;
+            _gameManager.OnGamePhaseChanged += OnGamePhaseChanged;
+            
+            gameObject.SetActive(false);
+            
+        }
+
+        private void OnGamePhaseChanged(GamePhase phase)
+        {
+            if (phase == GamePhase.Over) 
+                GameOver();
+        }
+
+        private void GameOver()
+        {
+            Player winner = _gameManager.Players.Find(player => player.IsAlive());
+            int turn = _gameManager.Turn;
+            int troops = winner.GetTroopsCountInTerritories();
+
+            string message = $"CONGRATULATIONS!\nPlayer {winner.Name} won in {turn} turns with {troops} troops left!";
+            message += "\ndefeated players:\n";
+            foreach (var player in _gameManager.Players)
+            {
+                if (player.IsDead())
+                    message += $"{player.Name}\n";
+            }
+
+            _gameOverInfoText.text = message;
+            _backgroundTintImage.gameObject.SetActive(true);
+            
+            gameObject.SetActive(true);
+        }
+    }
+}
